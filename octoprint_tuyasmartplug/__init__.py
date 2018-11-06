3c2188893b2d48b297d798db1a9c13e109d96ae5
ziirish <ziirish@ziirish.info>
2018-11-06 21:57:35 +0100

    this is the first version of the settings

--- octoprint_tuyasmartplug/__init__.py
+++ octoprint_tuyasmartplug/__init__.py
@@ -90,7 +90,7 @@ class tuyasmartplugPlugin(octoprint.plugin.SettingsPlugin,
 				self._tuyasmartplug_logger.setLevel(logging.INFO)
 
 	def get_settings_version(self):
-		return 5
+		return 1
 
 	def on_settings_migrate(self, target, current=None):
 		if current is None or current < self.get_settings_version():
