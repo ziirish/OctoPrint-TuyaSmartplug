2e0b0d180e2178cb78eaa6220e90147495b2fb00
ziirish <ziirish@ziirish.info>
2020-10-10 22:05:31 +0200

    chore: python3 compatibility (fixes #10 and #19)

--- octoprint_tuyasmartplug/__init__.py
+++ octoprint_tuyasmartplug/__init__.py
@@ -338,6 +338,8 @@ class tuyasmartplugPlugin(octoprint.plugin.SettingsPlugin,
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
 __plugin_name__ = "Tuya Smartplug"
+__plugin_version__ = "0.2.3"
+__plugin_pythoncompat__ = ">=2.7,<4"
 
 
 def __plugin_load__():
